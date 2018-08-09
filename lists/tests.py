7b7ef95a76cd864a7c4f906340011c6385be9f55
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-08-09 14:38:34 +0200

    Modified functional tests

--- lists/tests.py
+++ lists/tests.py
@@ -8,4 +8,8 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):        
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
+    
+    def test_can_save_a_POST_request(self):
+    	response = self.client.post('/', data={"item_text": "A new list Item"})
+    	self.assertIn("A new list Item", response.content.decode())
+    	self.assertTemplateUsed(response, "home.html")
