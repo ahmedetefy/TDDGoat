be56789197f59c253160b45112946803f4618f9d
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-08-10 16:25:37 +0200

    First steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,5 @@
 import time
-import unittest
+# import unittest
 
 from django.test import LiveServerTestCase
 
@@ -30,7 +30,6 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -40,13 +39,12 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        
 
         # She is invited to enter a to-do item straight away
-        input_box =  self.browser.find_element_by_id('id_new_item')
+        input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-        	input_box.get_attribute('placeholder'),
-        	"Enter a to-do item"
+            input_box.get_attribute('placeholder'),
+            "Enter a to-do item"
         )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -60,18 +58,16 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # enters "Use peacock feathers to make a fly"(Edith is very methodical)
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys("Use peacock feathers to make a fly")
         input_box.send_keys(Keys.ENTER)
-        
-
-
 
         # The page updates again, and now shows noth items on her list
-        
+
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
 
         # Satistfied, she goes back to sleep
 
@@ -89,12 +85,12 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Now a new User Francis, comes to the site
 
-        ## We use a new browser session to make sure that no information
-        ## off Edith's is coming through from cookies
+        # We use a new browser session to make sure that no information
+        # off Edith's is coming through from cookies
 
         self.browser.quit()
         self.browser = webdriver.Firefox()
-        
+
         # Francis visits the home page. There is no sign of Edith's list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
@@ -120,5 +116,28 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
+    def test_layout_and_styling(self):
+        # Edith goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
+        # She notices that the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
 
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
