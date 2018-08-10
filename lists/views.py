e2db1649baf0145794330e9902d01eedde6e5f0d
Ahmed Etefy <ahmed.etefy12@gmail.com>
2018-08-10 16:26:35 +0200

    PEP 8 Convention fixes

--- lists/views.py
+++ lists/views.py
@@ -4,22 +4,27 @@ from django.http import HttpResponse
 from .models import Item, List
 
 # Create your views here.
+
+
 def home_page(request):
     return render(request, "home.html")
 
+
 def view_list(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	return render(request, "list.html", {
+    list_ = List.objects.get(id=list_id)
+    return render(request, "list.html", {
         'list': list_
-        })
+    })
+
 
 def new_list(request):
     list_ = List.objects.create()
     item_text = request.POST['item_text']
     Item.objects.create(text=item_text, list=list_)
-    return redirect('/lists/'+ str(list_.id) + '/')
+    return redirect('/lists/' + str(list_.id) + '/')
+
 
 def add_item(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	Item.objects.create(text=request.POST['item_text'], list=list_)
-	return redirect('/lists/'+ str(list_id) + '/')
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/' + str(list_id) + '/')
